60ccef85fc009bf883cfbd0c57535dd66f5b237c
Audrey Carlsen <acarlsen@seattletimes.com>
2014-11-26 11:30:59 -0800

    Arrows don't show for first and last hero

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,6 @@ require("./filters");
 var isMobile = require("./isMobile");
 
 app.controller("PhotoController", ["$scope", "$filter", function($scope, $filter) {
-  
   var all = window.photoData;
   $scope.photos = [];
 
@@ -16,36 +15,31 @@ app.controller("PhotoController", ["$scope", "$filter", function($scope, $filter
   };
   $scope.showFilter = false;
 
+  var setHero = $scope.setHero = function(photo) {
+    $scope.uiState.hero = photo;
+    $scope.uiState.heroIndex = $scope.photos.indexOf(photo);
+    $scope.gotoAnchor(photo.id);
+  };
+
   $scope.$watch("tags", function() {
     var tagFilter = $filter("tagFilter");
     var filteredPhotos = tagFilter(all, $scope.tags);
-    if (!isMobile()) {
-      $scope.uiState.hero = filteredPhotos[0];
-    }
     $scope.photos = filteredPhotos;
+    if (!isMobile()) {
+      setHero($scope.photos[0]);
+    }
   }, true);
-
-  $scope.setHero = function(photo) {
-    $scope.uiState.hero = photo;
-  };
 
   $scope.clearHero = function() {
     $scope.uiState.hero = null;
   };
 
-  $scope.previousHero = function(photo) {
-    // var tagFilter = $filter("tagFilter");
-    // var filteredPhotos = tagFilter($scope.photos, $scope.tags);
-    // $scope.uiState.hero = photo;
-  };
-
   $scope.changeHero = function(delta) {
+    delta = delta || 1;
     var index = $scope.photos.indexOf($scope.uiState.hero);
-    var photo = $scope.photos[index + delta];
-    if (!photo) {
-      photo = $scope.photos[0];
-    }
-    $scope.uiState.hero = photo;
+    index += delta;
+    var nextPhoto = $scope.photos[index];
+    setHero(nextPhoto);
   };
 
   $scope.filterTitle = function() {

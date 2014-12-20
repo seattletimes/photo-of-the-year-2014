48c4df1fb7f82c78a7a48900fedfd8d89bacc7ab
Thomas Wilburn <news@thomaswilburn.net>
2014-12-19 16:24:38 -0800

    Added ids to the TSV file

--- tasks/getGallery.js
+++ tasks/getGallery.js
@@ -3,15 +3,16 @@ var htmlparser = require("htmlparser2");
 var request = require("request");
 var query = htmlparser.DomUtils.findAll;
 
-var url = "http://seattletimes.mycapture.com/mycapture/folder.asp?event=1883401&CategoryID=72513&ListSubAlbums=0&view=1&thisPage="
+var url = "http://seattletimes.mycapture.com/mycapture/folder.asp?event=1883401&CategoryID=72513&ListSubAlbums=0&view=1&thisPage=";
 
 module.exports = function(grunt) {
-
-  var links = [];
 
   grunt.registerTask("buylinks", function() {
 
     grunt.task.requires("photos");
+
+    var links = [];
+    var rows = new Array(grunt.data.photos.length);
 
     var done = this.async();
 
@@ -21,8 +22,10 @@ module.exports = function(grunt) {
         if (err) return c(err);
         htmlparser.parseDOM(body, function(err, document) {
           if (err) return c(err);
+          
           var html = document.filter(function(node) { return node.type == "tag" && node.name == "html" }).pop();
           if (!html) return c();
+
           query(function(element) {
             return element.name == "img" && element.parent.attribs.class == "MYCSmallPhotoBorder";
           }, document).forEach(function(img) {
@@ -40,20 +43,20 @@ module.exports = function(grunt) {
       if (err) return grunt.fatal(err);
 
       links.forEach(function(link) {
+        var title = link.title.replace(/[^a-z]/gi, "").slice(0, 20);
         for (var i = 0; i < grunt.data.photos.length; i++) {
           var photo = grunt.data.photos[i];
-          var caption = photo.caption.trim().slice(0, 20).replace(/[\s\W]/g, "");
-          //console.log(caption);
-          var title = link.title.trim().slice(0, 20).replace(/[\s\W]/g, "");
+          var caption = photo.caption.replace(/[^a-z]/gi, "").slice(0, 20);
+          // console.log(caption);
           if (caption == title) {
-            photo.buyID = link.id;
+            rows[i] = [photo.image, link.id, link.title];
             return;
           }
         }
-        console.log("NOT FOUND", link);
+        console.log("NOT FOUND", link, title);
       });
 
-      //console.log(grunt.data.photos);
+      grunt.file.write("temp/buy.csv", rows.map(function(r) { return r.join(",") }).join("\n"));
 
       done();
     });
